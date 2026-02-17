d670ba22b7d2835fd07a5619d457d33c0d8f722f
JasonLandbridge <jasonlandbridge@protonmail.com>
2026-02-18 00:35:28 +0100

    Fix the image modal

--- app/components/ImageModal.vue
+++ app/components/ImageModal.vue
@@ -7,8 +7,8 @@
         <NuxtImg
           :src="selectedImage?.src"
           :alt="selectedImage?.text"
-          width="2560"
-          height="1440"
+          width="1280"
+          height="720"
           class="block h-[85vh] max-w-[90vw] w-auto rounded-lg" />
         <div class="absolute bottom-0 left-0 right-0 rounded-b-lg bg-black/25 px-4 py-2">
           <p class="text-white text-sm text-center">
---
+++ app/components/content/ProseImg.vue
@@ -0,0 +1,33 @@
+<template>
+  <NuxtImg
+    :src="src"
+    :alt="alt"
+    :width="width"
+    :height="height"
+    class="cursor-pointer rounded-lg"
+    @click="openModal({ src, text: alt })" />
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  src: {
+    type: String,
+    default: '',
+  },
+  alt: {
+    type: String,
+    default: '',
+  },
+  width: {
+    type: [String, Number],
+    default: undefined,
+  },
+  height: {
+    type: [String, Number],
+    default: undefined,
+  },
+});
+
+const { src, alt } = toRefs(props);
+const { openModal } = useImageModal();
+</script>
